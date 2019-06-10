55ad9bde5e46b989cd124034cd062716bcc29852
longpdm-teko <long.pdm@teko.vn>
2019-06-10 15:22:28 +0700

    2

--- Long.py
+++ Long.py
@@ -2,3 +2,11 @@
 first = input("Enter first name: ")
 last = input("Enter last name: ")
 print(last + " " + first)
+
+# Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers
+numbers = input("Enter numbers separated by commas: ")
+list = numbers.split (",")
+tuple = tuple(list)
+print(list)
+print(tuple)
+
