62fd557d1a08e34afe6a23412ba798107c3666e3
longpdm-teko <long.pdm@teko.vn>
2019-06-10 17:28:04 +0700

    code practice 2

--- Long.py
+++ Long.py
@@ -33,3 +33,30 @@ def test_data_structure():
             print("Enter a smaller number")
 
     print(difference(18))
+
+# Write a Python program which accepts the radius of a circle from the user and compute the area
+from math import pi
+r = float(input("Input the radius : "))
+print ("The area of the circle is " + str(pi*r**2))
+
+# Write a Python program to display the first and last colors
+color_list = ["Red", "Green", "White", "Black"]
+print("%s %s" % (color_list[0], color_list[-1]))
+
+# Write a Python program to print the calendar of a given month and year
+import calendar
+Y = int(input("Enter the year: "))
+M = int(input("Enter the month: "))
+print(calendar.month(Y, M))
+
+# Write a Python program to print out a set containing all the colors from color_list_1 which are not present
+# in color_list_2.
+color_list_1 = set(["White", "Black", "Red"])
+color_list_2 = set(["Red", "Green"])
+print(color_list_1.difference(color_list_2))
+
+# Write a Python program that will accept the base and height of a triangle and compute the area
+b = int(input("Enter the base : "))
+h = int(input("Enter the height: "))
+area = (b*h)/2
+print(area)
