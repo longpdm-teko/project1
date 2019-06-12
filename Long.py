1aa3544d8c49269f3d0c45e8e90380dc49150245
longpdm-teko <long.pdm@teko.vn>
2019-06-12 14:35:40 +0700

    code 5

--- Long.py
+++ Long.py
@@ -102,7 +102,14 @@ def distinct_test(data):
 
 print(distinct_test([4, 2, 5, 4, 3]))
 
-
+# Write a Python program to check if a number is positive, negative or zero
+num = float(input("Enter a number: "))
+if num > 0:
+    print("Positive number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative number")
 
 
 
