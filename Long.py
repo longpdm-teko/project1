23db807190ee0fe5d0a5246ea5af31271f11d28f
longpdm-teko <long.pdm@teko.vn>
2019-06-12 14:34:21 +0700

    code 2

--- Long.py
+++ Long.py
@@ -77,6 +77,14 @@ for num in numbers:
     elif num % 2 == 0:
        print(num)
 
+# Write a Python program to add two objects if both objects are an integer type
+def add_numbers(a, b):
+    if not (isinstance(a,int) and isinstance(b,int)):
+        raise TypeError("Enter integers")
+    return a + b
+print(add_numbers(4,6))
+
+
 
 
 
