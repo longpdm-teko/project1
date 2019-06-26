bbfac5d0739a0921fc2d056911c73259f129fcb1
longpdm-teko <long.pdm@teko.vn>
2019-06-26 18:01:50 +0700

    test intern 2 fixed

--- test intern 1.py
+++ test intern 1.py
@@ -1,63 +1,142 @@
-class Chitietdon:
-    def __init__(self, name_don, weight, price):
-        self.name_don = name_don
+class ChiTiet:
+    def __init__(self, name):
+        self.name = name
+
+    def get_price(self):
+        pass
+
+    def get_weight(self):
+        pass
+
+    def __repr__(self):
+        return "{0}, price: {1}, weight: {2} ".format(self.name, self.get_price(), self.get_weight())
+
+
+class Chitietdon(ChiTiet):
+    def __init__(self, name, weight, price):
+        super(Chitietdon, self).__init__(name)
         self.weight = weight
         self.price = price
 
-    def __repr__(self):
-        return "Ma so chi tiet don: {0}, "\
-               "Trong luong: {1}, "\
-               "Gia thanh: {2}/////". format(self.name_don, self.weight, self.price)
+    def get_price(self):
+        return self.price
+
+    def get_weight(self):
+        return self.weight
 
 
-class Chitietphuc:
-    def __init__(self, name_phuc, list_items=[]):
-        self.name_phuc = name_phuc
+class Chitietphuc(ChiTiet):
+    def __init__(self, name, list_items=[]):
+        super(Chitietphuc, self).__init__(name)
         self.list_items = list_items
+
+    def get_price(self):
+        total = 0
+        for item in self.list_items:
+            total += item.get_price()
+        return total
+
+    def get_weight(self):
+        total = 0
+        for item in self.list_items:
+            total += item.get_weight()
+        return total
 
     def add_items(self, item):
         if not self.list_items:
             self.list_items = []
         self.list_items.append(item)
 
-    def __repr__(self):
-        return "Ma so chi tiet phuc: {0}, " \
-               "Cac chi tiet trong chi tiet phuc: {1}".format(self.name_phuc, self.list_items)
+
+# class Comaydon:
+#     def __init__(self, ten_may, list_may_don=[]):
+#         self.ten_may = ten_may
+#         self.list_may_don = list_may_don
+#
+#     def add_items(self, item):
+#         if not self.list_may_don:
+#             self.list_may_don = []
+#         self.list_may_don.append(item)
+#
+#     def __repr__(self):
+#         return "Ten may: {0}, " \
+#                "Cac chi tiet trong may: {1}".format(self.ten_may, self.list_may_don)
+#
+#
+# class Comayphuc:
+#     def __init__(self, ten_may, list_may_phuc=[]):
+#         self.ten_may = ten_may
+#         self.list_may_phuc = list_may_phuc
+#
+#     def add_items(self, item):
+#         if not self.list_may_phuc:
+#             self.list_may_phuc = []
+#         self.list_may_phuc.append(item)
+#
+#     def __repr__(self):
+#         return "Ten may: {0}, " \
+#                "Cac chi tiet trong may: {1}".format(self.ten_may, self.list_may_phuc)
 
 
-class Comay:
-    def __init__(self, ten_may, list_items=[]):
-        self.ten_may = ten_may
-        self.list_items = list_items
+# _list_may_don = [
+#     Comaydon("A1", [Chitietdon("CT001", 56, 47000), Chitietdon("CT002", 74, 75000), Chitietdon("CT003", 83, 56000)]),
+#     Comaydon("A2", [Chitietdon("CT004", 76, 88000), Chitietdon("CT005", 27, 38000), Chitietdon("CT006", 83, 93000)])
+# ]
+#
+# num = 1
+# print("List cac may toan chi tiet don: ")
+# for i in range(0, len(_list_may_don)):
+#     print("%s)" % num, _list_may_don[i])
+#     num = num + 1
+#     if num <= len(_list_may_don):
+#         continue
+#     else:
+#         break
+#
+# b = 0
+# for i in _list_may_don:
+#     for j in i.list_may_don:
+#         a = j.price
+#         b = a + b
+#     print("Tong gia tri cua may {0}: {1} vnd".format(i.ten_may, b))
+#
+# c = 0
+# for i in _list_may_don:
+#     for j in i.list_may_don:
+#         d = j.weight
+#         c = c + d
+#     print("Tong trong luong cua may {0}: {1} kg".format(i.ten_may, c))
+#
+# _list_may_phuc = [
+#     Comayphuc("B1", [Chitietdon("CT004", 73, 46000), Chitietphuc("PT2000", [Chitietdon("CT005", 47, 94000),
+#                                                                             Chitietdon("CT006", 85, 35000)])]),
+#     Comayphuc("B2", [Chitietphuc("PT3000", [Chitietdon("CT005", 83, 47000), Chitietdon("CT009", 73, 36000)]),
+#                      Chitietphuc("PT2000", [Chitietdon("CT005", 47, 94000), Chitietdon("CT006", 85, 35000)])])
+# ]
+#
+# print("")
+#
+# num = 1
+# print("List cac may gom chi tiet don va chi tiet phuc: ")
+# for i in range(0, len(_list_may_phuc)):
+#     print("%s)" % num, _list_may_phuc[i])
+#     num = num + 1
+#     if num <= len(_list_may_phuc):
+#         continue
+#     else:
+#         break
 
-    def add_items(self, item):
-        if not self.list_items:
-            self.list_items = []
-        self.list_items.append(item)
+d1 = Chitietdon("CT001", 73, 1000)
+d2 = Chitietdon("CT002", 73, 2000)
+d3 = Chitietdon("CT003", 73, 3000)
+d4 = Chitietdon("CT004", 73, 4000)
 
-    def __repr__(self):
-        return "Ten may: {0}, " \
-               "Cac chi tiet trong may: {1}".format(self.ten_may, self.list_items)
+p1 = Chitietphuc("B1", [d1, d2])
+p2 = Chitietphuc("B1", [d3, p1])
+p = Chitietphuc("B1", [p2, d4])
 
+print(p)
 
-_list = [
-    Comay("A1", [Chitietdon("CT001", 56, 47000), Chitietdon("CT002", 74, 75000), Chitietdon("CT003", 83, 56000)]),
-    Comay("A2", [Chitietdon("CT004", 76, 88000), Chitietdon("CT005", 27, 38000), Chitietdon("CT006", 83, 93000)]),
-    Comay("B1", [Chitietdon("CT004", 73, 46000), Chitietphuc("PT2000", [Chitietdon("CT005", 47, 94000),
-                                                                        Chitietdon("CT006", 85, 35000)])]),
-    Comay("B2", [Chitietphuc("PT3000", [Chitietdon("CT005", 83, 47000), Chitietdon("CT009", 73, 36000)]),
-                 Chitietphuc("PT2000", [Chitietdon("CT005", 47, 94000), Chitietdon("CT006", 85, 35000)])])
-]
-
-num = 1
-for i in range(0, len(_list)):
-    print("%s)" % num, _list[i])
-    num = num + 1
-    if num <= len(_list):
-        continue
-    else:
-        break
-
-
-
-
+# n = Chitietphuc("B1", [, Chitietphuc("PT2000", [Chitietdon("CT005", 47, 94000),
+#                                                                               Chitietdon("CT006", 85, 35000)])])
+# print(n.get_price())
---
+++ test intern 3.py
@@ -0,0 +1,104 @@
+class BangDiem:
+    def __init__(self, toan, ly, hoa):
+        self.__toan = toan
+        self.__ly = ly
+        self.__hoa = hoa
+
+    def get_diem_toan(self):
+        return self.__toan
+
+    def get_diem_ly(self):
+        return self.__ly
+
+    def get_diem_hoa(self):
+        return self.__hoa
+
+    def get_diem_tb(self):
+        return (self.get_diem_toan() + self.get_diem_ly() + self.get_diem_hoa()) / 3
+
+
+class SinhVien:
+    def __init__(self, id_num, name, age, bang_diem):
+        self.id_num = id_num
+        self.name = name
+        self.age = age
+        self.bang_diem = bang_diem
+
+    def get_age(self):
+        return self.age
+
+    def __get_id_num(self):
+        return self.id_num
+
+    def trung_binh(self):
+        return self.bang_diem.get_diem_tb()
+
+    def __repr__(self):
+        return "Ma so: {0}, " \
+               "Ho va Ten: {1}, " \
+               "Tuoi: {2}, " \
+               "Toan: {3} diem, " \
+               "Ly: {4} diem, " \
+               "Hoa: {5} diem, " \
+               "Diem trung binh: {6}/////".format(self.id_num, self.name, self.age, self.bang_diem.get_diem_toan(),
+                                                  self.bang_diem.get_diem_ly(), self.bang_diem.get_diem_hoa(),
+                                                  self.trung_binh())
+
+
+class LopHoc:
+    def __init__(self, name, list_sinh_vien=[]):
+        self.name = name
+        self.list_sinh_vien = list_sinh_vien
+
+    def add_sinh_vien(self, item):
+        if not self.list_sinh_vien:
+            self.list_sinh_vien = []
+        self.list_sinh_vien.append(item)
+
+    def get_avg_age(self):
+        total = 0
+        for item in self.list_sinh_vien:
+            total += item.get_age() # => total = total + item
+        total /= len(self.list_sinh_vien)  # => total = total / item
+        return total
+
+    def get_hs_gioi(self):
+        hsg = []
+        for item in self.list_sinh_vien:
+            if item.trung_binh() >= 8.0:
+                hsg.append(item)
+        return hsg
+
+    def __repr__(self):
+        return "Ten lop: {0}, " \
+               "Danh sach lop: {1}, " \
+               "Do tuoi trung binh: {2}, " \
+               "Danh sach hoc sinh gioi: {3}".format(self.name, self.list_sinh_vien, self.get_avg_age(),
+                                                     self.get_hs_gioi())
+
+
+bang_diem1 = BangDiem(6, 7, 8)  # 7.0
+bang_diem2 = BangDiem(7, 9, 8)  # 8.0
+bang_diem3 = BangDiem(5, 9, 5)  # 6.333
+bang_diem4 = BangDiem(8, 7, 10)  # 8.3
+bang_diem5 = BangDiem(7, 6, 10)  # 7.67
+
+sv1 = SinhVien("HS01", "Nguyen Van A", 16, bang_diem1)
+sv2 = SinhVien("HS02", "Nguyen Van B", 18, bang_diem2)
+sv3 = SinhVien("HS03", "Nguyen Van C", 15, bang_diem3)
+sv4 = SinhVien("HS04", "Nguyen Van D", 29, bang_diem4)
+
+
+# _list = [
+#     LopHoc("12A", [SinhVien("HS01", "Nguyen Van A", 16, 8, 6, 7), SinhVien("HS02", "Nguyen Van B", 17, 9, 9, 10),
+#                    SinhVien("HS03", "Nguyen Van C", 16, 5, 6, 2), SinhVien("HS04", "Nguyen Van D", 18, 4, 9, 6),
+#                    SinhVien("HS05", "Nguyen Van E", 17, 8, 10, 10), SinhVien("HS06", "Nguyen Van F", 18, 9, 7, 10),
+#                    SinhVien("HS07", "Nguyen Van G", 17, 5, 10, 8), SinhVien("HS08", "Nguyen Van H", 17, 10, 9, 10)])
+# ]
+
+lop_hoc = LopHoc("12A8", [sv1, sv2, sv3, sv4])
+
+for hsg in lop_hoc.get_hs_gioi():
+    print(hsg)
+
+print(lop_hoc.get_avg_age())
