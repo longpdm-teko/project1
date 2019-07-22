ee8e2f3a13c7f0273b7592d58776afb3ac0850f6
longpdm-teko <long.pdm@teko.vn>
2019-07-22 16:58:51 +0700

    Demo intern(4)

--- Demo.py
+++ Demo.py
@@ -1,3 +1,9 @@
+import datetime
+
+now = datetime.datetime.now()
+print("Welcome to car sorting program!")
+
+
 class Car:
     def __init__(self, model, year, mileage, title, price, distance, color, owners):
         self.model = model
@@ -30,29 +36,23 @@ class Car:
     def get_color(self):
         return self.color
 
-    def __get_owners(self):
-        return self.__owners
-
     def get_ownership(self):
-        if self.__get_owners() <= 2:
+        if self.__owners <= 2:
             return "Qualified"
         else:
             return "Disqualified"
 
-    def get_gas_per_mile(self):
-        pass
-
     def get_start_time(self):
         return "Car start time depends on their model"
+
+    def get_depreciation_rate(self):
+        return int(100 / (now.year - self.year)) * 2
 
 
 class Toyota(Car):
     def __init__(self, model, year, mileage, title, price, distance, color, owners, hot_chairs):
         super(Toyota, self).__init__(model, year, mileage, title, price, distance, color, owners)
         self.hot_chairs = hot_chairs
-
-    def get_gas_per_mile(self):
-        return (self.mileage / 100000) * 5
 
     def get_start_time(self):
         return "Toyota car starts in 3 seconds"
@@ -67,19 +67,16 @@ class Toyota(Car):
                "Color: {6}, " \
                "Ownership: {7}, " \
                "Hot chairs: {8}, " \
-               "Gas per mile: {9}, " \
-               "Start time: {10}".format(self.model, self.year, self.mileage, self.title, self.price, self.distance,
-                                         self.color, self.get_ownership(), self.hot_chairs, self.get_gas_per_mile(),
-                                         self.get_start_time())
+               "Start time: {9}, " \
+               "Depreciation rate: {10}%".format(self.model, self.year, self.mileage, self.title, self.price,
+                                                 self.distance, self.color, self.get_ownership(), self.hot_chairs,
+                                                 self.get_start_time(), self.get_depreciation_rate())
 
 
 class Ford(Car):
     def __init__(self, model, year, mileage, title, price, distance, color, owners, black_lights):
         super(Ford, self).__init__(model, year, mileage, title, price, distance, color, owners)
         self.black_lights = black_lights
-
-    def get_gas_per_mile(self):
-        return (self.mileage / 80000) * 6
 
     def get_start_time(self):
         return "Ford car starts in 2 seconds"
@@ -94,96 +91,49 @@ class Ford(Car):
                "Color: {6}, " \
                "Ownership: {7}, " \
                "Black lights: {8}, " \
-               "Gas per mile: {9}, " \
-               "Start time: {10}".format(self.model, self.year, self.mileage, self.title, self.price, self.distance,
-                                         self.color, self.get_ownership(), self.black_lights, self.get_gas_per_mile(),
-                                         self.get_start_time())
+               "Start time: {9}, " \
+               "Depreciation rate: {10}%".format(self.model, self.year, self.mileage, self.title, self.price,
+                                                 self.distance, self.color, self.get_ownership(), self.black_lights,
+                                                 self.get_start_time(), self.get_depreciation_rate())
 
 
-class Title:
-    def __init__(self, condition, seq):
-        self.condition = condition
-        self.seq = seq
-
-    def __repr__(self):
-        return "{}".format(self.condition)
-
-
-class Year:
-    def __init__(self, year):
-        self.year = year
-
-    def __repr__(self):
-        return "{}".format(self.year)
-
-
-class Color:
-    def __init__(self, name, seq):
-        self.name = name
-        self.seq = seq
-
-    def __repr__(self):
-        return "{}".format(self.name)
-
-
-_condition1 = Title("clean", 0)
-_condition2 = Title("damaged", 1)
-
-_year1 = Year(2014)
-_year2 = Year(2015)
-_year3 = Year(2016)
-_year4 = Year(2017)
-
-_color1 = Color('black', 0)
-_color2 = Color('silver', 1)
-_color3 = Color('gray', 2)
-_color4 = Color('gold', 3)
-_color5 = Color('white', 4)
-_color6 = Color('cream', 5)
-
-_car1 = Toyota("T1", _year2, 92441, _condition1, 10991, 5, _color3, 1, 3)
-_car2 = Ford("F1", _year1, 78150, _condition2, 10800, 6, _color1, 2, 4)
-_car3 = Toyota("T2", _year2, 44656, _condition1, 13791, 10, _color3, 4, 3)
-_car4 = Ford("F2", _year3, 88648, _condition2, 11650, 12, _color1, 3, 4)
-_car5 = Toyota("T3", _year3, 79857, _condition1, 11993, 20, _color2, 2, 3)
-_car6 = Ford("F3", _year2, 70533, _condition2, 12995, 39, _color2, 3, 4)
-_car7 = Toyota("T4", _year2, 58195, _condition1, 12495, 3, _color4, 1, 3)
-_car8 = Ford("F4", _year2, 86834, _condition2, 10691, 16, _color5, 2, 4)
-_car9 = Toyota("T5", _year4, 96541, _condition1, 11359, 18, _color6, 4, 3)
-_car10 = Ford("F5", _year1, 33452, _condition2, 12998, 22, _color6, 5, 4)
+def integer_input(title):
+    while True:
+        try:
+            result = int(input(title))
+            return result
+        except ValueError:
+            print("Enter numbers for this value!")
+            continue
 
 
 def user_input():
-    _input_brand = input("Enter Brand: ").upper()
-    if _input_brand == "Toyota".upper():
-        _input_model = input("Enter Model: ")
-        _input_year = int(input("Enter Year: "))
-        _input_mileage = int(input("Enter Mileage: "))
-        _input_title = input("Enter Title: ")
-        _input_price = int(input("Enter Price: "))
-        _input_distance = int(input("Enter Distance from home: "))
-        _input_color = input("Enter Color: ")
-        _input_owners = int(input("Enter Owners: "))
-        _input_hot_chairs = int(input("Enter Hot Chairs: "))
-        _Toyota_input = Toyota(_input_model, _input_year, _input_mileage, _input_title, _input_price, _input_distance,
-                               _input_color, _input_owners, _input_hot_chairs)
-        return _Toyota_input
-    elif _input_brand == "Ford".upper():
-        _input_model = input("Enter Model: ")
-        _input_year = int(input("Enter Year: "))
-        _input_mileage = int(input("Enter Mileage: "))
-        _input_title = input("Enter Title: ")
-        _input_price = int(input("Enter Price: "))
-        _input_distance = int(input("Enter Distance from home: "))
-        _input_color = input("Enter Color: ")
-        _input_owners = int(input("Enter Owners: "))
-        _input_black_lights = int(input("Enter Black Lights: "))
-        _Ford_input = Ford(_input_model, _input_year, _input_mileage, _input_title, _input_price, _input_distance,
-                           _input_color, _input_owners, _input_black_lights)
-        return _Ford_input
-    else:
-        print("Enter different brand!")
-        return user_input()
+    while True:
+        _input_brand = input("Enter Brand: ").upper()
+        a = "Toyota".upper()
+        b = "Ford".upper()
+        if _input_brand == a or _input_brand == b:
+            _input_model = input("Enter Model: ")
+            _input_year = integer_input("Enter Year: ")
+            _input_mileage = integer_input("Enter Mileage: ")
+            _input_title = input("Enter Title: ")
+            _input_price = integer_input("Enter Price: ")
+            _input_distance = integer_input("Enter Distance from home: ")
+            _input_color = input("Enter Color: ")
+            _input_owners = integer_input("Enter Owners: ")
+            if _input_brand == a:
+                _input_hot_chairs = integer_input("Enter Hot Chairs: ")
+                _Toyota_input = Toyota(_input_model, _input_year, _input_mileage, _input_title, _input_price,
+                                       _input_distance, _input_color, _input_owners, _input_hot_chairs)
+                return _Toyota_input
+            elif _input_brand == b:
+                _input_black_lights = integer_input("Enter Black Lights: ")
+                _Ford_input = Ford(_input_model, _input_year, _input_mileage, _input_title, _input_price,
+                                   _input_distance, _input_color, _input_owners, _input_black_lights)
+                return _Ford_input
+        else:
+            print("Enter a different brand!")
+            TypeError()
 
 
 def print_cars():
@@ -196,79 +146,60 @@ def print_cars():
         elif _input == "N".upper():
             return _list_cars
         else:
-            print("Type something else!")
+            print("Enter Y or N!")
+            TypeError()
 
 
-p = print_cars()
-
-
-def sort_price():
-    n = len(p)
-    for i in range(0, n - 1):
-        for j in range(0, n - i - 1):
-            if p[j].price > p[j + 1].price:
-                a = p[j]
-                p[j] = p[j + 1]
-                p[j + 1] = a
+def list_cars(list_car):
     num = 1
-    for car in p:
+    for car in list_car:
         print("%s) " % num, car)
         num += 1
-    return p
 
 
-def sort_mileage():
-    n = len(p)
+def sort(list_car, key):
+    n = len(list_car)
     for i in range(0, n - 1):
         for j in range(0, n - i - 1):
-            if p[j].mileage > p[j + 1].mileage:
-                a = p[j]
-                p[j] = p[j + 1]
-                p[j + 1] = a
-    num = 1
-    for car in p:
-        print("%s) " % num, car)
-        num += 1
-    return p
+            if getattr(list_car[j], key) > getattr(list_car[j + 1], key):
+                a = list_car[j]
+                list_car[j] = list_car[j + 1]
+                list_car[j + 1] = a
+    return list_car
 
 
-def sort_year():
-    n = len(p)
-    for i in range(0, n - 1):
-        for j in range(0, n - i - 1):
-            if p[j].year > p[j + 1].year:
-                a = p[j]
-                p[j] = p[j + 1]
-                p[j + 1] = a
-    num = 1
-    for car in p:
-        print("%s) " % num, car)
-        num += 1
-    return p
-
-
-def user_request():
+def export(list_car, key):
     while True:
-        _request = input("Enter request letter - A/sort by price, B/sort by mileage, C/sort by year, D/export to .txt, "
-                         "E/end program: ").upper()
-        if _request == "a".upper():
-            sort_price()
-        elif _request == "b".upper():
-            sort_mileage()
-        elif _request == "c".upper():
-            sort_year()
-        elif _request == "d".upper():
-            file = open("Demo.txt", "w")
-            file.write("My demo works!")
+        _export = input("Do you want to export this sorting to another file? Y/N: ").upper()
+        if _export == "Y".upper():
+            file = open(input("Enter file name: "), "w")
+            file.write(str(sort(list_car, key)))
             file.close()
-        elif _request == "e".upper():
+            print("%s exported successful!" % file.name)
+            break
+        elif _export == "N".upper():
+            return _key
+        else:
+            print("Enter Y or N!")
+            TypeError()
+
+
+if __name__ == '__main__':
+    p = print_cars()
+    while True:
+        _key = input("Enter price, mileage, or year to sort/ end to stop program: ")
+        if _key == "price" or _key == "mileage" or _key == "year":
+            sort(p, _key)
+            list_cars(p)
+            export(p, _key)
+        elif _key == "end":
+            print("Thanks for using this program!")
             break
         else:
-            print("Type letter of request!")
-            return user_request()
+            print("Enter request!")
+            TypeError()
 
 
-user_request()
 
 
 
